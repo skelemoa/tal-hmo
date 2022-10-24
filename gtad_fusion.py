739bb9d2f7bf7f93e9c4277bf1b1448e8ccf5007
Anurag Bagchi <40654905+miccooper9@users.noreply.github.com>
2022-10-24 15:43:52 +0800

    Update gtad_fusion.py

--- gtad_fusion.py
+++ gtad_fusion.py
@@ -5,14 +5,21 @@ import numpy as np
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--type', type=str, default='0', help='fusion type') # 0: AvgTrim 1: DupTrim 2: Concat
+parser.add_argument('--type', type=str, dest = 'type', default='0', help='fusion type') # 0: AvgTrim 1: DupTrim 2: Concat
+parser.add_argument('--vpath', type=str, dest = 'vpath', default='./rgb_val.h5', help='path to visual features')
+parser.add_argument('--apath', type=str, dest = 'apath', default='./audio_val.h5', help='path to audio features')
+parser.add_argument('--fusedpath', type=str, dest = 'fusedpath', default='./combined_val.h5', help='path to store fused features')
 opt = parser.parse_args()
 
 assert(int(opt.type) >= 0 and int(opt.type) <= 2)
 
-with h5py.File("combined_val.h5", "w") as writeHere:
-	with h5py.File('rgb_val.h5', "r") as rgbF:
-		with h5py.File('audio_val.h5', "r") as AudioF:
+vpath = opt.vpath
+apath = opt.apath
+fused_path = opt.fusedpath
+
+with h5py.File(fused_path, "w") as writeHere:
+	with h5py.File(vpath, "r") as rgbF:
+		with h5py.File(apath, "r") as AudioF:
 			for i in tqdm(rgbF.keys()):
 				videoFeats = torch.tensor(np.array(rgbF[i]))
 				audioFeats = torch.tensor(np.array(AudioF[i]))
